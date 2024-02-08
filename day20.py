97173a069e665283d51a4e0c30e8047af036500b
YulllJ <jyr8407@gmail.com>
2024-02-08 15:03:00 +0900

    v0.5 polynomier list structure

--- day20.py
+++ day20.py
@@ -1,40 +1,52 @@
+# 선형 리스트
+# 실제 메모리는 물리적 순서가 아니라 개념적 순서 그래서 배열을 id()로 찍어보면 순서가 없다는 것을 확인이 가능하다.
+# 가장 뒤에 넣어줄때는 공간 늘려주고 그 곳에 넣어주는것
+# 중간에 삽입하는 경우는 중간에 공간 확보해주고 밀어서 그 안에 공간을 만들어서 그 안에 넣어줄수 있도록 하는 것
+
+# 삽인 삭제는 오버헤드가 존재하지 않아
+# 다항식의 선형 리스트 표현
 '''
-import mymath as mm # as mm 안하면 그냥 fullname 다 적고 해야한다.
-import time # time을 재는 함수
-if __name__=="__main__":
-    n=int(input("Input n: "))
-    r=int(input("Input r: "))
-    print(f'{n}C{r}={mm.nCr(n,r)}')
+fx=[5,4,6,5]
+polyStr= "P(x)= "
+for i in range(len(fx)):
+    polyStr += f"+{fx[i]}x^{len(fx)-i}"
+print(polyStr)
 '''
-    # f=int(input())
-    # print(mm.factorial(f))
-# 컴퓨터에 따라 성능이 달라 동일 성능을 가졌다고 가정하고 시간 복잡도를 계산할때 횟수로 얘기를 하게됨
-# factorial 계산이 재귀함수 안쓰는게 훨씬 빨라
-# 재귀함수는 안에서 자기를 호출하는 형식
-# 운영체제가 레지스터에 많이 쓰는거 가져다놓고 스케쥴링을 하는 것 컴파일러는 최적의 코드로 짜주는것 빠르려면 레지스터랑 cpu랑 왔다갔다 해야됨
-# 재귀함수 호출 500번 넘어가면 컷다운함 (재귀함수는 창고에 있는 함수와 같아, 느리다)
-# 재귀함수는 똑같은 일을 하지 않음 같은게 있으면 return 을 시켜서 이전에 함수들이 받는다?
-# 성능은 무지하게 느려, depth때문에 안돌아가기도 RAM을 안쓰고 ALU로 계산해서 바로 써서 간단하지만 재귀함수는 아니라는 말인건가?
-# operation이 일어나는 횟수로 알고리즘의 성능을 측정한다.
-# big O, thetha , omega 등이 존재한다.(worst case를 기록해두는 것)
-# factorial이 제일 느리다.(O(n!))
-# 로그 밑이 2(bit단위라 0 아니면 1이니까)  단위가 operation(실행횟수)이니까 자연수이다.
-# 숫자 맞추기 게임 0에서 100까지 게임 몇번만에 맞췄는지 테스트
-import random
-target=random.randrange(1,100)
-count=0
-while True:
-    mine=int(input("숫자를 입력하세요"))
-    if target == mine:
-        print("숫자를 맞혔습니다!")
-        count += 1
-        print(f'{count}번째에 맞혔습니다')
-        break
-    elif target!= mine:
-        count += 1
-        if target >mine :
-            print("up")
-        elif target < mine :
-            print("down")
-    else :
-        print("다시 입력하세요")
+
+## 함수 선언 부분 ##
+def print_poly(f_x) -> str:#refactor rename하면 이름 추천받아서 해줌(파이썬식 이름으로 한번에 바꿔줘?)
+    term = len(f_x) - 1
+    poly_expression = "P(x) = " #스네이크 이름법이 아냐??
+
+    for i in range(len(fx)):
+        coef = f_x[i]  # 계수
+
+        if (coef >= 0):
+            poly_expression += "+"
+        poly_expression += f"{coef}x^{term} "
+        term -= 1
+
+    return poly_expression
+
+
+def calc_poly(xVal, f_x):
+    return_value = 0
+    term = len(f_x) - 1  # 최고차항 숫자 = 배열길이-1
+
+    for i in range(len(fx)):
+        coef = f_x[i]  # 계수
+        return_value += coef * xVal ** term
+        term -= 1
+
+    return return_value
+
+
+## 전역 변수 선언 부분 ##
+fx = [-8,-1,3,2,0,5]
+
+## 메인 코드 부분 ##
+if __name__ == "__main__":
+    print(print_poly(fx))
+    print(calc_poly(int(input("X 값-->")), fx))
+
+
